0a488e7b2b4120f43e14df3eb5acd2a10dc8321d
krupakar0307 <krupakarreddy.yasa@gmailc.om>
2025-02-02 18:25:57 +0000

    fix fmt

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -9,14 +9,14 @@ terraform {
   }
 }
 module "lambda" {
-  aws_region            = "us-west-2"
-  environment           = var.environment
-  source                = "../modules/lambda-mod"
+  aws_region                = "us-west-2"
+  environment               = var.environment
+  source                    = "../modules/lambda-mod"
   lambda_function_file_path = "${path.module}/../../service"
-  lambda_function_name  = "lambda_function"
-  handler               = "lambda_function.lambda_handler"
-  runtime               = "python3.12"
-  create_bedrock_policy = false
+  lambda_function_name      = "lambda_function"
+  handler                   = "lambda_function.lambda_handler"
+  runtime                   = "python3.12"
+  create_bedrock_policy     = false
   create_s3_policy          = false
   # environment_variables = {
   #     "BEDROCK_RESOURCE_ARN" = var.bedrock_resource_arn

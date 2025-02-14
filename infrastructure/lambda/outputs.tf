6778817c3b22e5c31fc53c20566b0b89a62c121c
Krupakar Reddy <shirishareddy.yasa@gmail.com>
2025-02-14 14:28:59 +0530

    push

--- infrastructure/lambda/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value = module.lambda.name
-}
-output "aws_lambda_function_url" {
-  value = module.lambda.aws_lambda_function_url
-}
-output "aws_iam_role" {
-  value = module.lambda.aws_iam_role
-}
--- infrastructure/lambda/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "environment" {
-  description = "Environment for the lambda function"
-  type        = string
-  default     = ""
-
-}
